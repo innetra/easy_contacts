83117dba9eae6682a73462d1ed311aa696c08900
Ivan Torres <ivan@innetra.com>
2009-01-21 17:39:43 -0600

    support for fields_for prefixes in emails

--- generators/easy_contacts/templates/helpers/emails_helper.rb
+++ generators/easy_contacts/templates/helpers/emails_helper.rb
@@ -1,5 +1,14 @@
 module EmailsHelper
-  def place_email_form(owner)
-    render :partial => "emails/emails", :locals => { :owner => owner }
+  def place_email_form(record_or_name, *args)
+    options = {}
+    case record_or_name
+      when String, Symbol
+        options[:fields_prefix] = "#{record_or_name}[email_attributes][]"
+        owner = args.first
+      else
+        owner = record_or_name
+        options[:fields_prefix] = "#{dom_class(owner)}[email_attributes][]"
+    end
+    render :partial => "emails/emails", :locals => { :owner => owner, :options => options }
   end
 end
